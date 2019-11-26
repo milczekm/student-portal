4a1028b37186585cd0052e5d621289d99aa944de
milczekm <marcin.milczek@poczta.pl>
2019-11-26 21:54:22 +0100

    Student Base

--- src/main/java/com/students/controller/StudentController.java
+++ src/main/java/com/students/controller/StudentController.java
@@ -1,7 +1,7 @@
 package com.students.controller;
 
 import com.students.model.Student;
-import com.students.service.StudentServiceImpl;
+import com.students.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,7 +20,7 @@ public class StudentController {
         return "welcome";
     }
     @Autowired
-    private StudentServiceImpl studentService;
+    private StudentService studentService;
 
 
     @RequestMapping(value = "/students", method = RequestMethod.GET)
---
+++ src/main/java/com/students/model/Address.java
@@ -0,0 +1,4 @@
+package com.students.model;
+
+public class Address {
+}
